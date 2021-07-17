4172f58532f12cab77abc6270e9fd70946c13149
fikridzkr <fikridzakir179@gmail.com>
2021-07-17 15:21:34 +0700

    finish redux-tookit

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import './Counter.css';
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  incrementHandler,
-  increaseHandler,
-  decrementHandler,
-  toggleCounterHandler,
-} from '../actions/CounterAction';
+import { counterActions } from './CounterSlice';
 const Home = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
   const showCounter = useSelector((state) => state.showCounter);
 
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(5));
+  };
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggle());
+  };
   return (
     <div className="counter-div">
       <h1>Counter</h1>
       {showCounter && <h5>{counter}</h5>}
-      <button onClick={() => incrementHandler(dispatch)}>Tambah</button>
-      <button onClick={() => increaseHandler(dispatch)}>Tambah 5</button>
-      <button onClick={() => decrementHandler(dispatch)}>Kurang</button>
-      <button onClick={() => toggleCounterHandler(dispatch)}>
-        Toggle Counter
-      </button>
+      <button onClick={() => incrementHandler()}>Tambah</button>
+      <button onClick={() => increaseHandler()}>Tambah 5</button>
+      <button onClick={() => decrementHandler()}>Kurang</button>
+      <button onClick={() => toggleCounterHandler()}>Toggle Counter</button>
     </div>
   );
 };
